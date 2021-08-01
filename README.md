f834979b022b8f08109030a074517f06222fd403
orthegavalentin <iravadu@gmail.com>
2021-08-01 15:12:45 +0200

    addition of question 1 sub-sums(Dont' remember the question well)

--- README.md
+++ README.md
@@ -3,3 +3,18 @@
 a collection of coding game challenges
 
 
+Question 1) print all numbers below or equal an integer number n  that adds up to  n
+
+for example if the number is 4
+
+4
+3 1
+2 2
+2 1 1
+1 1 1 1
+
+
+the solution of the problem is not quiet complete given that i dont 
+remember well how the question was asked
+
+
--- src/App.java
+++ src/App.java
@@ -1,5 +1,42 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        computerSums(4);
+    }
+
+    /**
+     * 
+     * Question number 1
+     * 
+     * We use a while loop inside two nested for loops
+     * 
+     * 
+     * 
+     */
+
+    public static void computerSums(int n) {
+        int temp = 0;
+
+        for (int i = n; i > 0; i--) {
+            for (int j = 1; j <= n; j++) {
+                String res = "i=" + i;
+                temp = i + j;
+                if (i == n) {
+                    System.out.println(res);
+                    break;
+
+                }
+
+                while (temp <= n) {
+                    res += "  " + j;
+                    if (temp == n)
+                        System.out.println(res);
+                    temp += j;
+                }
+
+            }
+
+        }
+
     }
 }
